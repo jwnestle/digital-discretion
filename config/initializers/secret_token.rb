5946c43e91b188826070816aa460a4510edfba9b
Jason White <jasonwhitestl@me.com>
2012-04-09 20:45:50 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalDiscretion::Application.config.secret_token = 'a507427efe45895955d725d9d36200673b820451e68cccd46ec6e31bfc8e0e49a74c44682d00ca7c6e41e569718c5eead869f27279e75ea0e461d4495b4e80c9'
